7dcda3bfff1376e8a54d01d509e6478425446c8b
Bartosz Malec <bartosz.malec@onet.pl>
2024-05-05 13:04:28 +0200

    Slider mobile support

--- scripts/slider.js
+++ scripts/slider.js
@@ -15,41 +15,44 @@ function specificSlide(imageIndex) {
 	if (activeIndex === imageIndex)
 		return;
 	
-	markImageAsActive(imagesArray[imageIndex], imageIndex);
+	markAsActive(imagesArray[imageIndex], imageIndex);
 	const futureImageIndex = getNextElementIndex(projectImages, imageIndex);
-	markImageAsNext(projectImages[futureImageIndex])
+	markAsNext(projectImages[futureImageIndex])
 	
-	imagesArray.forEach((element, index) => {
-		if (index === imageIndex || index === futureImageIndex)
-			return;
-		
-		markImageAsFuture(element);
-	});
-	
+	marksAsFutureWithExceptions(imageIndex, futureImageIndex);
 	updateSlides(imageIndex);
 }
 
 function nextSlide() {
+	showSlide(true);
+}
+
+function previousSlide() {
+	showSlide(false);
+}
+
+function showSlide(next) {
 	const activeIndex = imagesArray.findIndex(image => image.classList.contains("active"));
 	
-	const newActiveIndex = getNextElementIndex(projectImages, activeIndex);
-	markImageAsActive(projectImages[newActiveIndex], newActiveIndex)
-	markImageAsNext(getFutureElement(projectImages, activeIndex));
-	markImageAsFuture(imagesArray[activeIndex])
+	const newActiveIndex = next ? getNextElementIndex(projectImages, activeIndex) : getPreviousElementIndex(projectImages, activeIndex);
+	const newNextIndex = getNextElementIndex(projectImages, newActiveIndex);
+	
+	markAsActive(projectImages[newActiveIndex], newActiveIndex)
+	markAsNext(projectImages[newNextIndex]);
+	next ? markAsFuture(imagesArray[activeIndex]) : marksAsFutureWithExceptions(newActiveIndex, newNextIndex);
 	
 	updateSlides(newActiveIndex);
+}
+
+function getPreviousElementIndex(array, activeIndex) {
+	return activeIndex > 0 ? activeIndex - 1 : array.length - 1;
 }
 
 function getNextElementIndex(array, activeIndex) {
 	return activeIndex < array.length - 1 ? activeIndex + 1 : 0;
 }
 
-function getFutureElement(array, activeIndex) {
-	const nextIndex = (activeIndex + 2) % array.length;
-	return array[nextIndex];
-}
-
-function markImageAsActive(image, detailsActiveIndex) {
+function markAsActive(image, detailsActiveIndex) {
 	image.classList.remove("next");
 	image.classList.remove("future");
 	image.classList.add("active");
@@ -63,16 +66,25 @@ function markImageAsActive(image, detailsActiveIndex) {
 	detailsArray[detailsActiveIndex].classList.add("active");
 }
 
-function markImageAsNext(image) {
+function markAsNext(image) {
 	image.classList.remove("active");
 	image.classList.remove("future");
 	image.classList.add("next");
 }
 
-function markImageAsFuture(image) {
+function markAsFuture(image) {
 	image.classList.remove("active");
 	image.classList.remove("next");
 	image.classList.add("future");
+}
+
+function marksAsFutureWithExceptions(index1, index2) {
+	imagesArray.forEach((element, index) => {
+		if (index === index1 || index === index2)
+			return;
+		
+		markAsFuture(element);
+	});
 }
 
 function updateSlides(activeIndex) {
@@ -90,15 +102,63 @@ function updateSlides(activeIndex) {
 
 jQuery(document).ready(function($){
 	let $timeline_block = $('.section_2_inner');
+	let $slider = $('#slider');
 	
-	if ($($timeline_block).offset().top > $(window).scrollTop()+$(window).height()*0.75)
+	if ($($timeline_block).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
 		$($timeline_block).addClass('is-hidden');
+		$($slider).addClass('is-hidden');
+	}
 	
 	
 	$(window).on('scroll', function(){
 		
-		if($($timeline_block).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $($timeline_block).hasClass('is-hidden') )
+		if($($timeline_block).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $($timeline_block).hasClass('is-hidden')) {
 			$($timeline_block).removeClass('is-hidden').addClass('slideBlob');
+			$($slider).removeClass('is-hidden').addClass('slideBlob');
+		}
 		
 	});
 });
+
+
+
+const handleTouchStart = (evt) => {
+	const firstTouch = getTouches(evt)[0];
+	xDown = firstTouch.clientX;
+	yDown = firstTouch.clientY;
+};
+
+const handleTouchMove = (evt) => {
+	if (!xDown || !yDown) return;
+	
+	const xUp = evt.touches[0].clientX;
+	const yUp = evt.touches[0].clientY;
+	
+	const xDiff = xDown - xUp;
+	const yDiff = yDown - yUp;
+	
+	if (Math.abs(xDiff) > Math.abs(yDiff)) {
+		if (xDiff > 0) {
+			console.log("Right swipe detected!");
+			nextSlide();
+		} else {
+			previousSlide();
+		}
+	} else {
+		if (yDiff > 0) {
+			/* down swipe */
+		} else {
+			/* up swipe */
+		}
+	}
+	/* reset values */
+	xDown = null;
+	yDown = null;
+};
+
+const getTouches = (evt) => {
+	return evt.touches || evt.originalEvent.touches;
+};
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
